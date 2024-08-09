2e91abe27e3dd12f0161fe6bf349302e8b9559a1
hmertakyatan <122562336+hmertakyatan@users.noreply.github.com>
2024-08-09 21:25:31 +0300

    Delete utils/axis_fixer.py

--- utils/axis_fixer.py
+++
@@ -1,6 +0,0 @@
-import pymupdf
-def convert_y_coordinate_pymupdf_to_pypdf(rect, page_height_pts):
-    temp = page_height_pts - rect.y1
-    rect.y1 = page_height_pts - rect.y0
-    rect.y0 = temp
-    return rect
